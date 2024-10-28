e6b2b4f19f3fbb5a75f47a53ffdb341370f4d534
keeptrain <19211020@bsi.ac.id>
2024-10-28 18:51:01 +0700

    feat(repository): Implement reply repository

---
+++ src/Domains/replies/ReplyRepository.js
@@ -0,0 +1,19 @@
+class ReplyRepository {
+  async addRepliesToComment() {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getReplyById() {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getRepliesOnCommentById() {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteReply() {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = ReplyRepository;
