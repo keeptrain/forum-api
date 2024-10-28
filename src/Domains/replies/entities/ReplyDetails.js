268a031b9c18aca476acaa5f3602ff437b50720b
keeptrain <19211020@bsi.ac.id>
2024-10-28 19:40:53 +0700

    feat(entity): implement Add & Details for Reply entities

---
+++ src/Domains/replies/entities/ReplyDetails.js
@@ -0,0 +1,28 @@
+class ReplyDetails {
+  constructor(payload) {
+    this._verifyPayload(payload);
+
+    const {
+      id, content, date, username,
+    } = payload;
+
+    this.id = id;
+    this.content = content;
+    this.date = date;
+    this.username = username;
+  }
+
+  _verifyPayload({
+    id, content, date, username,
+  }) {
+    if (!id || !content || !date || !username) {
+      throw new Error('REPLY_DETAILS.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof id !== 'string' || typeof content !== 'string' || typeof date !== 'string' || typeof username !== 'string') {
+      throw new Error('REPLY_DETAILS.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
+}
+
+module.exports = ReplyDetails;
