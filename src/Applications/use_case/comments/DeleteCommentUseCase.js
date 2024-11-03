43d1627211730b2a05a189aa160211229a8303c7
keeptrain <19211020@bsi.ac.id>
2024-11-03 07:25:44 +0700

    refactor(usecase): change some code for using a entities

--- src/Applications/use_case/comments/DeleteCommentUseCase.js
+++ src/Applications/use_case/comments/DeleteCommentUseCase.js
@@ -1,10 +1,12 @@
+const DeleteComment = require('../../../Domains/comments/entities/DeleteComment');
+
 class DeleteCommentUseCase {
   constructor({ commentRepository }) {
     this._commentRepository = commentRepository;
   }
 
   async execute(useCasePayload) {
-    const { id, owner } = useCasePayload;
+    const { id, owner } = new DeleteComment(useCasePayload);
     await this._commentRepository.checkCommentAvailability(id);
     await this._commentRepository.verifyCommentOwner(id, owner);
     await this._commentRepository.deleteCommentById(useCasePayload.id);
--- src/Applications/use_case/replies/DeleteReplyUseCase.js
+++ src/Applications/use_case/replies/DeleteReplyUseCase.js
@@ -1,10 +1,12 @@
+const DeleteReply = require('../../../Domains/replies/entities/DeleteReply');
+
 class DeleteReplyUseCase {
   constructor({ replyRepository }) {
     this._replyRepository = replyRepository;
   }
 
   async execute(useCasePayload) {
-    const { id, owner } = useCasePayload;
+    const { id, owner } = new DeleteReply(useCasePayload);
     await this._replyRepository.verifyReplyAvailability(id);
     await this._replyRepository.verifyReplyOwner(id, owner);
     await this._replyRepository.deleteReplyById(useCasePayload.id);
